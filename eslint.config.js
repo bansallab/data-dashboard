d34f4cbd59243447582ea11dfa6d83f79f0d75f9
Andrew Tiu <63322884+winter-again@users.noreply.github.com>
2025-05-05 11:36:49 -0400

    Fix ESLint + prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
 import globals from "globals";
-
+import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
-    { files: ["**/*.{js,mjs,cjs}"] },
     {
         files: ["**/*.{js,mjs,cjs}"],
-        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+        plugins: { js },
+        extends: ["js/recommended"],
+    },
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        languageOptions: { globals: globals.browser },
     },
     eslintConfigPrettier,
 ]);
+
